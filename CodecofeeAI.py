894e8002a5669003147a115062596fec189f9991
The-voidx <newnexuxdev@gmail.com>
2026-04-15 05:24:03 -0300

    concertei umas coisas adiciobei otras

--- CodecofeeAI.py
+++ CodecofeeAI.py
@@ -1,24 +1,58 @@
-import os, ollama, time
+import os, time, platform
+from openai import OpenAI
+from os import getenv
 
-os.system("clear")
+def limpar():
+    if platform.system() == "Windows":
+        os.system("clear")
+    
+    elif platform.system() == "Linux":
+        os.system("clear")
+    else:
+        print("Infelizmente seu dispositivo não e compativel na nossa lista de dispositivos compativeis...")
 
-user = input("Qual seria o seu nome: ")
-print(f"\n e um prazer conhece-lo {user}\n")
-time.sleep(1.0)
-ianam = input("Qual nome vc quer dar a sua IA?: ")
+limpar()
+
+
+try:
+    user = input("Qual seria o seu nome: ")
+    print(f"\n e um prazer conhece-lo {user}\n")
+    time.sleep(1.0)
+    ianam = input("Qual nome vc quer dar a sua IA?: ")
+except:
+    print("Tivemos um erro no nosso servidor, por favor reinicie")
+
 
 time.sleep(2.0)
-os.system("clear")
-
+limpar()
 
 while True:
-    #caixa de pergunta ou area das variaveis
-    quest = input(f"{user}|>>> ")
-    
-    #codigo do funcionamento da ia
-    resposta_completa = ollama.chat(model='llama3.2:1b', messages=[{'role': 'user', 'content': quest}])
 
-    texto_da_ia = resposta_completa['message']['content']
+# gets API Key from environment variable OPENAI_API_KEY
+    quest = input(f"[{user}]>>>")
 
-    #mensagem ou impressões
-    print(f"\n" + str(ianam) + "]--> " +texto_da_ia + "\n")
+    client = OpenAI(
+      base_url="https://openrouter.ai/api/v1",
+      api_key=getenv("OPENROUTER_API_KEY"),
+    )
+    completion = client.chat.completions.create(
+      model="nvidia/nemotron-3-super-120b-a12b:free",
+      extra_headers={
+        "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+        "X-OpenRouter-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+    },
+    # pass extra_body to access OpenRouter-only arguments.
+    # extra_body={
+      # "models": [
+      #   "${Model.GPT_4_Omni}",
+      #   "${Model.Mixtral_8x_22B_Instruct}"
+      # ]
+     # },
+    messages=[
+       {
+         "role": "user",
+         "content": quest,
+       },
+      ],
+    ) 
+    print(completion.choices[0].message.content)
