3f2474e4d33e9236f56088a71140eaaa3226d8ca
The-voidx <newnexuxdev@gmail.com>
2026-04-18 21:42:50 -0300

    eu adicionei a bliblioteca sys, novoss objetivos, arrumei e fiz a ia lembrar do nosso nome e do nme dela, adicionei um sistema de reset, agora ele lé um arquvo txt que contem a api, novas mensagens de erri e como arrumalos, troca de modelo de ia saindo do da nvidia pra um modelo da openai, adicionei espaço ao texto enviado pela ia.

--- CodecofeeAI.py
+++ CodecofeeAI.py
@@ -1,6 +1,10 @@
-import os, time, platform #adicionar uma forma de baixar todas as depedencias automaticamente e uma interface CLI bem feita
+import os, time, platform, sys #adicionar uma forma de baixar todas as depedencias automaticamente e uma interface CLI bem feita e adicionar uma interface grafica sem ser CLI
 from openai import OpenAI
 from os import getenv
+
+def reset():
+    os.execv(sys.executable, [sys.executable] + sys.argv)
+
 
 def limpar():
     if platform.system() == "Windows":
@@ -10,17 +14,23 @@ def limpar():
         os.system("clear")
     else:
         print("Infelizmente seu dispositivo não e compativel na nossa lista de dispositivos compativeis...")
-        time.sleep(4.0)
+        time.sleep(2.0)
         os.system("exit")
 
 limpar()
 
 try:
   if getenv("OPENROUTER_API_KEY") == None:
-    Api_key = input("Nos informe a sua key, pode ser achada no openrouter: ")
-    os.environ["OPENROUTER_API_KEY"] = Api_key
+    Api_key = open("Config.txt")
+    recept = Api_key.read()
+    Api_key.close()
+    os.environ["OPENROUTER_API_KEY"] = recept
 except:
     print("infelizmente recebemos algum erro no nosso codigo tente rodar novamente...")
+    limpar()
+
+    print("Talvez você possa editar isso nas configurações de api em configurações -> configurações de api -> api personalizada.")#tenho plano de criar um painel de configurações que vai salvar tudo em um arquivo no caso o Config.txt, onde vai fcar os chats e as configurações e tals ate pra mudar o modelo da ia
+    reset()
 
 try:
     time.sleep(0.2)
@@ -37,11 +47,11 @@ limpar()
 
 client = OpenAI(
       base_url="https://openrouter.ai/api/v1",
-      api_key=getenv("OPENROUTER_API_KEY"),#adicionar uma forma de baixar a key sozinha ou executar
+      api_key=getenv("OPENROUTER_API_KEY"),
     )
 
 #memoria da IA
-historico=[{"role": "system", "content": "Falar apenas Portugues brasil", }]#melhorar esse sistem ainda esta muinto basico
+historico=[{"role": "system", "content": f"Falar apenas Portugues brasil; Seu nome é {ianam}; meu nome é {user}", }]#melhorar esse sistem ainda esta muinto basico
 
 while True:
 
@@ -51,7 +61,7 @@ while True:
     historico.append({"role": "user", "content": quest})
      
     completion = client.chat.completions.create(
-      model="nvidia/nemotron-3-super-120b-a12b:free",
+      model="openai/gpt-4o-mini-2024-07-18",
       extra_headers={
         "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
         "X-OpenRouter-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
@@ -69,5 +79,5 @@ while True:
     ) 
    
 
-    print(str(ianam) + "~≳ " + completion.choices[0].message.content)
+    print( str(f"\n{ianam}") + "~≳ " + completion.choices[0].message.content + str("\n"))
 
